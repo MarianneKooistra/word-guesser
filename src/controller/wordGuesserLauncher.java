c8f25cd2c1e8c8ef0136edfdf2ef9804e91b992b
Marianne <Marianne-Kooistra@hotmail.com>
2024-07-01 18:27:06 +0200

    Initial commit

---
+++ src/controller/wordGuesserLauncher.java
@@ -0,0 +1,12 @@
+package controller;
+
+/*
+* @author Marianne Kooistra 
+* A little game to guess a words, like Wordle, Lingo or Mastermind.
+*/
+
+public class wordGuesserLauncher {
+    public static void main(String[] args) {
+        
+    }
+}
