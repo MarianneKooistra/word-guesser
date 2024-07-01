6d3c7c89f627eb0d6d10d4750a1ef83b940fdaae
Marianne <Marianne-Kooistra@hotmail.com>
2024-07-01 18:28:00 +0200

    Basic version with message output and user input in the terminal

--- src/controller/wordGuesserLauncher.java
+++ src/controller/wordGuesserLauncher.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.Scanner;
+
 /*
 * @author Marianne Kooistra 
 * A little game to guess a words, like Wordle, Lingo or Mastermind.
@@ -7,6 +9,18 @@ package controller;
 
 public class wordGuesserLauncher {
     public static void main(String[] args) {
-        
+        String wordToGuess;
+        wordToGuess = "four";
+
+        Scanner keyboardScanner = new Scanner(System.in);
+        String userGuess;
+        System.out.println("\n**********");
+        System.out.println("Give your best guess:");
+
+        userGuess = keyboardScanner.nextLine();
+        keyboardScanner.close();
+
+        System.out.println("The word was ... \t" + wordToGuess + "!");
+        System.out.println("Your guess was ... \t" + userGuess);
     }
 }
